6ee48bf737d9cd391727b63c4ada4f54c1d749a8
youruilin <youruilin@gmail.com>
2025-02-11 15:02:42 +0800

    fix: test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import generatedRoutes from 'virtual:generated-pages'
 import { userStore } from '@/stores/user'
-
-// 动态获取 basePath，确保开发环境和生产环境一致
-const basePath = import.meta.env.MODE === 'production' ? '/vue3-ts-shuangzhao/' : '/'
-
 const routes = [
   ...generatedRoutes,
   {
@@ -14,7 +10,7 @@ const routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHistory(basePath),
+  history: createWebHistory(),
   routes
 })
 
@@ -25,7 +21,6 @@ router.beforeEach((to, from, next) => {
   if (store.token) {
     // 检查是否为首页路径，如果是则跳转到 /task
     if (to.path === '/') {
-      console.log(basePath)
       next('/task')
     } else {
       next()
@@ -36,11 +31,8 @@ router.beforeEach((to, from, next) => {
       to.path === '/login/ServiceAgree' ||
       to.path === '/login/PrivatePolicy'
     ) {
-      console.log(basePath)
       next()
     } else {
-      console.log(basePath)
-
       next('/login')
     }
   }
