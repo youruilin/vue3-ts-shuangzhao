22fca56073436aba19a870ac6719d6af468b2e34
youruilin <youruilin@gmail.com>
2025-02-11 12:35:59 +0800

    feat: 测试工作流

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="node" />
+
+// src/types/vite-plugin-eslint.d.ts
+declare module 'vite-plugin-eslint' {
+  const value: unknown
+  export = value
+}
