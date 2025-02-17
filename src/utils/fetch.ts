afa7ae0962a1faead636f93c5188bdf051c577b4
youruilin <youruilin@gmail.com>
2025-02-17 15:17:13 +0800

    fix: 调改 fetch 路径

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,23 +1,13 @@
 // src/utils/fetchWithBaseUrl.js
-const fetchWithBaseUrl = (url, options?) => {
+const fetchWithBaseUrl = url => {
   // 获取环境变量中的 PUBLIC_URL
-  const baseUrl = import.meta.env.PUBLIC_URL || '' // import.meta.env 也可以获取环境变量
+  const baseUrl = import.meta.env.PUBLIC_URL || '/' // import.meta.env 也可以获取环境变量
 
   // 拼接完整的请求路径
   const fullUrl = `${baseUrl}${url}`
 
   // 调用原生的 fetch 方法
-  return fetch(fullUrl, options)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
-      }
-      return response.json()
-    })
-    .catch(error => {
-      console.error('Fetch error:', error)
-      throw error
-    })
+  return fetch(fullUrl)
 }
 
 export default fetchWithBaseUrl
