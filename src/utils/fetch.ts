fef179311489165cda8b8216eec243e6ad1bd05a
youruilin <youruilin@gmail.com>
2025-02-17 16:38:57 +0800

    fix: banner

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,12 +1,9 @@
-// src/utils/fetchWithBaseUrl.js
 const fetchWithBaseUrl = url => {
-  // 获取环境变量中的 VITE_PUBLIC_URL，默认值为 '/'
-  const baseUrl = import.meta.env.VITE_PUBLIC_URL || '/'
-  console.log('baseUrl', baseUrl)
+  // 获取环境变量中的 VITE_PUBLIC_URL
+  const baseUrl = import.meta.env.VITE_PUBLIC_URL || '/' // 默认值为根路径（开发环境）
 
-  // 确保 baseUrl 以 / 结尾
+  // 拼接完整的请求路径
   const fullUrl = `${baseUrl.endsWith('/') ? baseUrl : baseUrl + '/'}${url}`
-  console.log('fullUrl', fullUrl)
 
   // 调用原生的 fetch 方法
   return fetch(fullUrl)
